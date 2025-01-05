64d41f5e7626a59c4ef868f500604cb3c2fcf28e
djsreeraj <sreeraj011m@gmail.com>
2025-01-05 20:05:42 +0530

    added header, nav

---
+++ src/ProductDemo.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+
+const ProductDemo = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default ProductDemo
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">About</li>
+        <li className="navbar-item">Products</li>
+        <li className="navbar-item">Support</li>
+        <li className="navbar-item">Consulting</li>
+        <li className="navbar-item">Quick Demo</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
